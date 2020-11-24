6c96c42a7caeeb53a188b805f5788413b85aacac
unknown <ck169@hdm-stuttgart.de>
2020-11-24 10:34:02 +0100

    Attribute angepasst, Kommentare zur Besprechung ergänzt

--- src/ProjectAdministration.py
+++ src/ProjectAdministration.py
@@ -6,11 +6,12 @@ class ProjectAdministration():
     """
     Person-Methoden
     """
-    def create_person(self):
+    # Attribute bei allen create nochmals betrachten
+    def create_person(self, name:str):
         """Eine Person anlegen"""
         person = Person()
 
-        with PersonMapr() as mapper:
+        with PersonMapper() as mapper:
             return mapper.insert(person)
 
     def save_person(self, person):
@@ -18,12 +19,12 @@ class ProjectAdministration():
         with PersonMapper() as mapper:
             mapper.update(person)
 
-    def delete_person(self):
+    def delete_person(self, person):
         """Person löschen"""
         with PersonMapper() as mapper:
             mapper.delete(person)
 
-    def get_person_by_id(self):
+    def get_person_by_id(self, person_id):
         """Die Person mit ihrer gegebenen ID auslesen"""
         with PersonMapper() as mapper:
             return mapper.find_by_id(id)
@@ -32,15 +33,16 @@ class ProjectAdministration():
         """Alle Personen ausgeben"""
         with PersonMapper() as mapper:
 
-    def get_person_by_name(self):
+    def get_person_by_name(self, name):
         """Die Person mit ihrem Namen auslesen"""
         with PersonMapper() as mapper:
     
-    def add_role_to_person(self):
+    def add_role_to_person(self, role, person):
         """Der Person eine Rolle zuteilen"""
         with PersonMapper() as mapper:
     
-    def remove_role_from_person(self):
+    # Was macht remove genau? Unterschied zu delete?
+    def remove_role_from_person(self, role, person):
         """Die Rollenzuteilung einer Person entfernen"""
         with PersonMapper() as mapper:
     
@@ -54,15 +56,15 @@ class ProjectAdministration():
 
         with StudentMapper() as mapper:
     
-    def save_student(self):
+    def save_student(self, student):
         """Einen Studenten speichern"""
         with StudentMapper() as mapper:
     
-    def delete_student(self):
+    def delete_student(self, student):
         """Den gewählten Studenten löschen"""
         with StudentMapper() as mapper:
     
-    def get_student_by_id(self):
+    def get_student_by_id(self, student_id):
         """Den Studenten mit seiner gegebenen ID auslesen"""
         with StudentMapper() as mapper:
     
@@ -70,15 +72,15 @@ class ProjectAdministration():
         """Alle Studenten ausgeben"""
         with StudentMapper() as mapper:
 
-    def get_student_by_name(self):
+    def get_student_by_name(self, name):
         """Den Student mit seinem Namen auslesen"""
         with StudentMapper() as mapper:
 
-    def get_student_by_matriculation_number(self):
+    def get_student_by_matriculation_number(self, matriculation_number):
         """Den Student mit seiner Matrikelnummer auslesen"""
         with StudentMapper() as mapper:
     
-    def get_student_by_course_abbreviation(self):
+    def get_student_by_course_abbreviation(self, course_abbreviation):
         """Den Student mit seinem Studiengangskürzel auslesen"""
         with StudentMapper() as mapper:
     
@@ -92,15 +94,15 @@ class ProjectAdministration():
 
         with SemesterMapper() as mapper:
 
-    def save_semester(self):
+    def save_semester(self, semester):
         """Ein Semester speichern"""
         with SemesterMapper() as mapper:
 
-    def delete_semester(self):
+    def delete_semester(self, semester):
         """Ein Semester löschen"""
         with SemesterMapper() as mapper:
 
-    def get_semester_by_id(self):
+    def get_semester_by_id(self, semester_id):
         """Ein Semester mit seiner gegebenen ID auslesen"""
         with SemesterMapper() as mapper:
 
@@ -108,15 +110,15 @@ class ProjectAdministration():
         """Alle Semester ausgeben"""
         with SemesterMapper() as mapper:
 
-    def get_semester_by_name(self):
+    def get_semester_by_name(self, name):
         """Alle Semester mit ihren Namen ausgeben"""
         with SemesterMapper() as mapper:
 
-    def add_project_to_semester(self):
+    def add_project_to_semester(self, project, semester):
         """Ein Projekt einem Semester hinzufügen"""
         with SemesterMapper() as mapper:
 
-    def remove_project_from_semester(self):
+    def remove_project_from_semester(self, project, semester):
         """Ein Projekt von einem Semester entfernen"""
         with SemesterMapper() as mapper:
     
@@ -130,15 +132,15 @@ class ProjectAdministration():
 
         with RoleMapper() as mapper:
     
-    def save_role(self)
+    def save_role(self, role)
         """Eine Rolle speichern"""
         with RoleMapper() as mapper:
 
-    def delete_role(self):
+    def delete_role(self, role):
         """Eine Rolle löschen"""
         with RoleMapper() as mapper:
     
-    def get_role_by_id(self):
+    def get_role_by_id(self, role_id):
         """Eine Rolle anhand ihrer ID auslesen"""
         with RoleMapper() as mapper:
         
@@ -149,22 +151,22 @@ class ProjectAdministration():
     """
     Automat-Methoden
     """
-
+# !!! Kann Automat erstellt werden?!!!
     def create_automat(self):
         """Einen Automaten anlegen"""
         automat = Automat()
 
         with AutomatMapper() as mapper:
     
-    def save_automat(self):
+    def save_automat(self, automat):
         """Einen Automaten speichern"""
         with AutomatMapper() as mapper:
 
-    def delete_automat(self):
+    def delete_automat(self, automat):
         """Einen Automaten löschen"""
         with AutomatMapper() as mapper:
     
-    def get_automat_by_id(self):
+    def get_automat_by_id(self, automat_id):
         """Einen Automaten anhand seiner ID auslesen"""
         with AutomatMapper() as mapper:
     
@@ -183,15 +185,15 @@ class ProjectAdministration():
 
         with ParticipationMapper() as mapper:
     
-    def save_participation(self):
+    def save_participation(self, participation):
         """Eine Teilnahme speichern"""
         with ParticipationMapper() as mapper:
 
-    def delete_participation(self):
+    def delete_participation(self, participation):
         """Eine Teilnahme löschen"""
         with ParticipationMapper() as mapper:
 
-    def get_participation_by_id(self):
+    def get_participation_by_id(self, participation_id):
         """Eine Teilnahme anhand ihrer ID auslesen"""
         with ParticipationMapper() as mapper:
     
@@ -199,19 +201,19 @@ class ProjectAdministration():
         """Alle Teilnahmen ausgeben"""
         with ParticipationMapper() as mapper:
     
-    def add_student_to_participation(self):
+    def add_student_to_participation(self, student, participation):
         """Ein Student einer Teilnahme hinzufügen"""
         with ParticipationMapper() as mapper:
     
-    def remove_student_from_participation(self):
+    def remove_student_from_participation(self, student, participation):
         """Ein Student von einer Teilnahme entfernen"""
         with ParticipationMapper() as mapper:
     
-    def add_grading_to_participation(self):
+    def add_grading_to_participation(self, grading, participation):
         """Eine Bewertung einer Teilnahme hinzufügen"""
         with ParticipationMapper() as mapper:
 
-    def remove_grading_from_participation(self):
+    def remove_grading_from_participation(self, grading, participation):
         """Eine Bewertung von einer Teilnahme entfernen"""
         with ParticipationMapper() as mapper:
     
@@ -226,15 +228,15 @@ class ProjectAdministration():
 
         with GradingMapper() as mapper:
     
-    def save_grading(self):
+    def save_grading(self, grading):
         """Eine Bewertung speichern"""
         with GradingMapper() as mapper:
 
-    def delete_grading(self):
+    def delete_grading(self, grading):
         """Eine Bewertung löschen"""
         with GradingMapper() as mapper:
     
-    def get_grading_by_id(self):
+    def get_grading_by_id(self, grading_id):
         """Eine Bewertung anhand ihrer ID auslesen"""
         with GradingMapper() as mapper:
 
@@ -253,15 +255,15 @@ class ProjectAdministration():
 
         with ProjectMapper() as mapper:
     
-    def save_project(self):
+    def save_project(self, project):
         """Ein Projekt speichern"""
         with ProjectMapper() as mapper:
     
-    def delete_project(self):
+    def delete_project(self, project):
         """Ein Projekt löschen"""
         with ProjectMapper() as mapper:
     
-    def get_project_by_id(self):
+    def get_project_by_id(self, project_id):
         """Ein Projekt anhand seiner ID auslesen"""
         with ProjectMapper() as mapper:
     
@@ -269,34 +271,37 @@ class ProjectAdministration():
         """Alle Projekte ausgeben"""
         with ProjectMapper() as mapper:
     
-    def get_project_by_name(self):
+    def get_project_by_name(self, name):
         """Ein Projekt anhand seines Namens auslesen"""
         with ProjectMapper() as mapper:
     
-    def add_person_to_project(self):
+    # Student oder Person?
+    def add_person_to_project(self, person, project):
         """Eine Person einem Projekt hinzufügen"""
         with ProjectMapper() as mapper:
     
-    def remove_person_from_project(self):
+    def remove_person_from_project(self, person, project):
         """Eine Person von einem Projekt entfernen"""
         with ProjectMapper() as mapper:
     
-    def add_participation_to_project(self):
+    def add_participation_to_project(self, participation, project):
         """Eine Teilnahme einem Projekt hinzufügen"""
         with ProjectMapper() as mapper:
     
-    def remove_participation_from_project(self):
+    def remove_participation_from_project(self, participation, project):
         """Eine Teilnahme von einem Projekt entfernen"""
         with ProjectMapper() as mapper:
     
-    def add_semester_to_project(self):
+    def add_semester_to_project(self, semester, project):
         """Ein Semester einem Projekt hinzufügen"""
         with ProjectMapper() as mapper:    
     
-    def remove_semester_from_project(self):
+    def remove_semester_from_project(self, semester, project):
         """Ein Semester von einem Projekt entfernen"""
         with ProjectMapper() as mapper:
     
+    #Macht das Sinn?
+    """
     def add_module_to_project(self):
         """Ein Modul einem Projekt hinzufügen"""
         with ProjectMapper() as mapper:
@@ -305,18 +310,20 @@ class ProjectAdministration():
         """Ein Modul von einem Projekt entfernen"""
         with ProjectMapper() as mapper:
     
-    def add_projecttype_to_project(self):
+    """
+    
+    def add_projecttype_to_project(self, project_type, project):
         """Ein Projekttyp einem Projekt hinzufügen"""
         with ProjectMapper() as mapper:
     
-    def remove_projecttype_from_project(self):
+    def remove_projecttype_from_project(self, project_type, project):
         """Ein Projekttyp von einem Projekt entfernen"""
         with ProjectMapper() as mapper:
     
     """
     ProjectType-Methoden
     """
-
+# !!! Bei ProjectType aufpassen (2x t), Abgleich mit Datenbank
     def create_projecttype(self):
         """Einen Projekttyp anlegen"""
 
@@ -324,15 +331,15 @@ class ProjectAdministration():
 
         with ProjectTypeMapper() as mapper:
     
-    def save_projecttype(self):
+    def save_projecttype(self, project_type):
         """Einen Projekttyp speichern"""
         with ProjectTypeMapper() as mapper:
     
-    def delete_projecttype(self):
+    def delete_projecttype(self, project_type):
         """Einen Projekttyp löschen"""
         with ProjectTypeMapper() as mapper:
     
-    def get_projecttype_by_id(self):
+    def get_projecttype_by_id(self,project_type_id):
         """Einen Projekttyp anhand seiner ID auslesen"""
         with ProjectTypeMapper() as mapper:
     
@@ -340,7 +347,7 @@ class ProjectAdministration():
         """Alle Projekttypen ausgeben"""
         with ProjectTypeMapper() as mapper:
     
-    def get_projecttype_by_name(self):
+    def get_projecttype_by_name(self name):
         """Einen Projekttyp anhand seines Namen auslesen"""
         with ProjectTypeMapper() as mapper:
     
@@ -355,15 +362,15 @@ class ProjectAdministration():
 
         with StateTypeMapper() as mapper:
     
-    def save_state(self):
+    def save_state(self, state):
         """Einen Status speichern"""
         with StateTypeMapper() as mapper:
     
-    def delete_state(self):
+    def delete_state(self, state):
         """Einen Status löschen"""
         with StateTypeMapper() as mapper:
 
-    def get_state_by_id(self):
+    def get_state_by_id(self, self_id):
         """Einen Status anhand seiner ID auslesen"""
         with StateTypeMapper() as mapper:
     
@@ -382,15 +389,15 @@ class ProjectAdministration():
 
         with ModuleMapper() as mapper:
     
-    def save_module(self):
+    def save_module(self, module):
         """Ein Modul speichern"""
         with ModuleMapper() as mapper:
 
-     def delete_module(self):
+     def delete_module(self, module):
          """Ein Modul löschen"""
         with ModuleMapper() as mapper:
     
-     def get_module_by_id(self):
+     def get_module_by_id(self, module_id):
          """Ein Modul anhand seiner ID auslesen"""
         with ModuleMapper() as mapper:
     
@@ -398,18 +405,18 @@ class ProjectAdministration():
          """Alle Module ausgeben"""
         with ModuleMapper() as mapper:
     
-     def get_module_by_name(self):
+     def get_module_by_name(self, name):
          """Ein Modul anhand seines Namen auslesen"""
         with ModuleMapper() as mapper:
     
-     def get_module_by_edv_number(self):
+     def get_module_by_edv_number(self, edv_number):
          """Ein Modul anhand seiner EDV-Nummer ausgeben"""
         with ModuleMapper() as mapper:
     
-     def add_project_to_module(self):
+     def add_project_to_module(self, project, module):
          """Ein Projekt einem Modul hinzufügen"""
         with ModuleMapper() as mapper:
     
-     def remove_project_from_module(self):
+     def remove_project_from_module(self, project, module):
          """Ein Projekt von einem Modul entfernen"""
         with ModuleMapper() as mapper:
