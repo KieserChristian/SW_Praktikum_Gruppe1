5efe4a88bdbb27d800c04311ff474cb95d64ba23
DanielErbele <73832542+DanielErbele@users.noreply.github.com>
2020-11-30 12:03:05 +0100

    BusinessObject hinzugefügt (ProjectType, Student und Project

---
+++ src/bo/nbo/Project.py
@@ -0,0 +1,119 @@
+from NamedBusinessObjects import NamedBusinessObjects
+from Automat import Argument
+from ProjectType import ProjectType
+
+class Project(NamedBusinessObjects, Automat):
+
+    transdisziplinaer = ProjectType("Transdiziplinäres Projekt", l0, 20) 
+    interdisziplinaer = ProjectType( "Interdisziplinäres Projekt", 5, 10)
+    fachspezifisch = ProjectType("Fachspezifisches Projekt", 3, 5)
+
+    def __init__(self):
+        super().__init__()
+         self.__name = name
+         self.__capacity = 0
+         self.__external_partners = ""
+         self.__short_description = ""
+         self.__weekly_flag = False
+         self.__bd_before_lecture_period = 0
+         self.__bd_in_exam_period = 0
+         self.__bd_preferred_in_lecture_period = 0
+         self.__special_room = ""
+
+    def get_name(self):
+        """Name auslesen"""
+        return self.__name
+
+    def get_capacity(self):
+        """Kapazität auslesen"""
+        return self.__capacity
+
+    def get_external_partners(self):
+        """externe Partner auslesen"""
+        return self.__external_partners
+
+    def get_short_description(self):
+        """Kurzbeschreibung auslesen"""
+        return self.__short_description
+
+    def get_weekly_flag(self):
+        """Flag auslesen"""
+        return self.__weekly_flag
+
+    def get_bd_before_lecture_period(self):
+        """Werktage vor Vorlesungszeit auslesen"""
+        return self.__bd_before_lecture_period
+
+    def get_bd_in_exam_period(self):
+        """Werktage in Prüfungszeit auslesen"""
+        return self.__bd_in_exam_period
+
+    def get_bd_in_lecture_period(self):
+        """Werktage in Vorlesungszeit auslesen"""
+        return self.__bd_in_lecture_period
+
+    def get_bd_preferred_in_lecture_period(self):
+        """präferierte Werktage in Vorlesungszeit auslesen"""
+        return self.__bd_preferred_in_lecture_period
+
+    def get_special_room(self):
+        """Spezieller Raum auslesen"""
+        return self.__special_room
+
+    def set_name(self, name):
+        """Name setzen"""
+        self.__name = name
+
+    def set_capacity(self, capacity):
+        """Kapazität setzen"""
+        self.__capacity = capacity
+
+    def set_external_partners(self, external_partners):
+        """Externe Partner setzen"""
+        self.__external_partners = external_partners
+
+    def set_short_descripton(self, short_descripton):
+        """Kurzbeschreibung setzen"""
+        self.__short_descripton = short_descripton
+
+    def set_weekly_flag(self, weekly_flag):
+        """Flag setzen"""
+        self.__weekly_flag = weekly_flag
+
+    def set_bd_before_lecture_period(self, bd_before_lecture_period):
+        """Werktage vor Vorlesungszeit setzen"""
+        self.__bd_before_lecture_period = bd_before_lecture_period
+    
+    def set_bd_in_exam_period(self, bd_in_exam_period):
+        """Werktage in Prüfungszeit setzen"""
+        self.__bd_in_exam_period = bd_in_exam_period  
+
+    def set_bd_in_lecture_period(self, bd_in_lecture_period):
+        """Werktage in Vorlesungszeit setzen"""
+        self.__bd_in_lecture_period = bd_in_lecture_period  
+
+    def set_bd_preferred_in_lecture_period(self, bd_preferred_in_lecture_period):
+        """präferierte Werktage in Vorlesungszeit setzen"""
+        self.__bd_preferred_in_lecture_period = bd_preferred_in_lecture_period  
+
+    def set_special_room(self, special_room):
+        """Spezieller Raum setzen"""
+        self.__special_room = special_room
+
+@staticmethod
+def from_dict(dictionary=dict()):
+    """Umwandeln eines Python dict () """
+    project = Project()
+    project.set__id(dictionary["id"])
+    project.set__creation_date(dictionary["creation_date"])
+    project.set_name(dictionary["name"])
+    project.set_capacity(dictionary["capacity"])
+    project.set_external_partners(dictionary["external_partners"])
+    project.set_short_description(dictionary["short_description"])
+    project.set_weekly_flag(dictionary["weekly_flag"])
+    project.set_bd_before_lecture_period(dictionary["bd_before_lecture_period"])
+    project.set_bd_in_exam_period(dictionary["bd_in_exam_period"])
+    project.set_bd_in_lecture_period(dictionary["bd_in_lecture_period"])
+    project.set_bd_preferred_in_lecture_period(dictionary["bd_preferred_in_lecture_period"])
+    project.set_special_room(dictionary["special_room"])
+    return project
---
+++ src/bo/nbo/Student.py
@@ -0,0 +1,34 @@
+from Person import Person
+
+class Student(Person):
+
+    def __init__(self):
+        super.__init__(self)
+        self.__matriculation_number = 0
+        self.__course_abbreviation = 0
+
+    def get_matriculation_number(self):
+        """Immatrikulationsnummer auslesen"""
+        return self.__matriculation_number
+
+    def set_matriculation_number(self, value):
+        """Immatrikulationsnummer setzen"""
+        self.__matriculation_number = value
+
+    def get_course_abbreviation(self):
+        """Kurs-Kürzel auslesen"""
+        return self.__edv_number
+
+    def set_coure_abbreviation(self, value):
+        """Kurs-Kürzel setzen"""
+        self.__course_abbreviation = value
+
+@staticmethod
+def from_dict(dictionary=dict()):
+    """Umwandeln eines Python dict () """
+    student = Student()
+    student.set__id(dictionary["id"])
+    student.set__creation_date(dictionary["creation_date"])
+    student.set_matriculation_number(dictionary["matriculation_number"])
+    student.set_course_abbreviation(dictionary["course_abbreviation"])
+    return student
