b152337311bac7ef1b39416d02f087fcb40459b7
Daniel.Weinert <dw073@hdm-stuttgart.de>
2020-12-01 11:58:45 +0100

    App.js angepasst zu den Anforderungen für die firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import logo from './logo.svg';
+import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import Themen from "./Theme";
@@ -21,3 +23,25 @@ class App extends React.Component {
     };
 
 }
+function App () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
