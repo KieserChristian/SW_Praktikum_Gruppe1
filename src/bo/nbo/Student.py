5cb47d7d4655739d5a88b995c351e3e07fff7808
DanielErbele <73832542+DanielErbele@users.noreply.github.com>
2020-12-02 11:20:50 +0100

    id und creation_date zu Klasse Student hinzugefügt

--- src/bo/nbo/Student.py
+++ src/bo/nbo/Student.py
@@ -1,11 +1,14 @@
 from Person import Person
+from datetime import datetime
 
 class Student(Person):
 
     def __init__(self):
-        super.__init__(self)
+        super.__init__()
         self.__matriculation_number = 0
         self.__course_abbreviation = 0
+        self.__id = 0
+        self.__creation_date =datetime.datetime.now()
 
     def get_matriculation_number(self):
         """Immatrikulationsnummer auslesen"""
@@ -22,6 +25,22 @@ class Student(Person):
     def set_coure_abbreviation(self, value):
         """Kurs-Kürzel setzen"""
         self.__course_abbreviation = value
+
+    def get_id (self):
+        """Auslesen der ID"""
+        return self.__id
+
+    def get_creation_date(self):
+        """Auslesen des Erstelldatums"""
+        return self.__creation_date
+
+    def set_id (self, value):
+        """ID setzen"""
+        self.__id = value 
+
+    def set_creation_date (self, creation_date):
+        """Erstellungsdatum setzen"""
+        self.__creation_date = creation_date
 
 @staticmethod
 def from_dict(dictionary=dict()):
