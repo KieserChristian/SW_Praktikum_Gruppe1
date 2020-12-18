e7ae34949df36e143fa00cd96b7ef9a15a1cca59
unknown <ck169@hdm-stuttgart.de>
2020-12-18 11:18:20 +0100

    App.js und index.js zurückgeschraubt, damit Frontend wieder funktioniert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import firebase from "firebase/app";
-import "firebase/auth";
-import Themen from "./Theme";
+//import firebase from "firebase/app";
+//import "firebase/auth";
+//import Themen from "./Theme";
 //import der Components
-import firebaseConfig from "./firebaseconfig";
+//import firebaseConfig from "./firebaseconfig";
 
 
-class App extends React.Component {
-
-    //Constructor of the App, which initializes firebase */
-    constructor(props) {
-        super (props);
-
-        //Init empty state 
-        this.state = {
-            currentUser: null,
-            appError: null,
-            authError: null,
-            authLoading: false
-        }; 
-    };
-
-}
-function App() {	
-    return (	
-      <div className="App">	
-        <header className="App-header">	
-          <img src={logo} className="App-logo" alt="logo" />	
-          <p>	
-            Edit <code>src/App.js</code> and save to reload.	
-          </p>	
-          <a	
-            className="App-link"	
-            href="https://reactjs.org"	
-            target="_blank"	
-            rel="noopener noreferrer"	
-          >	
-            Learn React	
-          </a>	
-        </header>	
-      </div>	
-    );	
-  }	
-  
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Test
+          </a>
+        </header>
+      </div>
+    );
+  }
   
   export default App;
