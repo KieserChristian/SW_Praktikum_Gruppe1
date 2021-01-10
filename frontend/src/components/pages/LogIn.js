b2499fe00611417724816f075c1e1dc791849953
YusufGoegues <73108892+YusufGoegues@users.noreply.github.com>
2021-01-10 14:59:28 +0100

    Frontend: LogInView Admin- Button ausgearbeitet und Verlinkung hinzugefügt. AdminView groben Aufbau eingefügt.

--- frontend/src/components/pages/LogIn.js
+++ frontend/src/components/pages/LogIn.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { Button, Grid, Typography, withStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+// import { Link as RouterLink } from 'react-router-dom'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class LogIn extends React.Component {
 
@@ -8,13 +11,32 @@ class LogIn extends React.Component {
     }
 
     render() {
+        
 
         return(
+            
         <div>
+           <center> 
             <Button variant="contained">Student</Button>
             <Button variant="contained">Dozent</Button>
-            <Button variant="contained">Admin</Button>
+            
+                <Link to='/AdminView'>
+                <Button
+                    size= "large"
+                    variant="contained"
+                    color="default"
+                    
+                 >
+                            Admin
+                  
+                </Button>
+                </Link>
+        </center>        
         </div>
+         
+        
+        
+        
         );
     }
 }
