fe272f5521844ff3a8c574fd2e73b9089ee85b80
Christina Kaden <ck171@hdm-stuttgart.de>
2021-01-31 23:45:43 +0100

    Tabelle in StudentGradingView.js überarbeitet, Durchstich in StudentGradingEntry.js für Projekt, Modul, EDV-Nummer, ECTS und SWS geschafft, Styling und Einrückungen angepasst

--- frontend/src/components/StudentGradingEntry.js
+++ frontend/src/components/StudentGradingEntry.js
@@ -2,116 +2,107 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import ProjectAdminAPI from '../api/ProjectAdminAPI';
 import {withStyles } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
-import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-
+import TableCell from '@material-ui/core/TableCell';
 
 class StudentGradingEntry extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            project: this.props.propproject,
-            grading: null,
-            error: null,
-            module: null,
-            projectType: null,
-        } 
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: props.project,
+      module: null,
+      projectType: null,
+      error: null
+    } 
+  }
 
-    getModuleById = () => {
-        ProjectAdminAPI.getAPI().getModuleById(this.state.project.getModuleId())
-        .then(moduleNBO => {
-            this.setState({
-            module: moduleNBO,
-            loadingProgress: false,
-            error: null
-          });
-        }).catch(e => {
-          this.setState({
-            module: null,
-            loadingInProgress: false,
-            error: e
-          })
-        });
-        this.setState({
-        loadingInProgress: true,
-        error: null
-        });
-    }
-   
-    getProjectTypeById = () => {
-        ProjectAdminAPI.getAPI().getProjectTypeById(this.state.project.getProjectTypeId())
-        .then(projectTypeAPI => {
-            this.setState({
-            projectType: projectTypeAPI,
-            loadingProgress: false,
-            error: null
-          });
-        }).catch(e => {
-          this.setState({
-            projectType: null,
-            loadingInProgress: false,
-            error: e
-          })
-        });
-        this.setState({
-        loadingInProgress: true,
-        error: null
-        });
-    }
-    getGradingByParticipationId = (participationId) => {
-        ProjectAdminAPI.getAPI().getGradingByParticipationId(participationId)
-        .then(gradingAPI => {
-            this.setState({
-            grading: gradingAPI,
-            loadingProgress: false,
-            error: null
-          });
-        }).catch(e => {
-          this.setState({
-            grading: null,
-            loadingInProgress: false,
-            error: e
-          })
-        });
-        this.setState({
-        loadingInProgress: true,
-        error: null
-        });
-    }
-    
-   getData = async() => {
-        let participation = await this.getParticipationByProjectId()
-        this.getGradingByParticipationId(participation.getId())
-   }
-    componentDidMount() {
-        this.getData();
-        this.getProjectTypeById();
-        this.getModuleById();        
-    }
+  getModuleById = () => {
+    ProjectAdminAPI.getAPI().getModuleById(this.state.project.getModuleId())
+    .then(moduleNBO => {
+      this.setState({
+        module: moduleNBO
+      });
+    }).catch(e => {
+      this.setState({
+        module: null,
+        loadingInProgress: false,
+        error: e
+      })
+    });
+    this.setState({
+      loadingInProgress: true,
+      error: null
+    });
+  }
 
-    render() {
-        const { classes } = this.props;
-        const { gradings, module, project, projectType } = this.state;
+  getProjectTypeById = () => {
+    ProjectAdminAPI.getAPI().getProjectTypeById(this.state.project.getProjectTypeId())
+    .then(projectTypeNBO => {
+        this.setState({
+        projectType: projectTypeNBO,
+        loadingInProgress: false,
+        error: null
+      });
+    }).catch(e => {
+      this.setState({
+        projectType: null,
+        loadingInProgress: false,
+        error: e
+      })
+    });
+    this.setState({
+    loadingInProgress: true,
+    error: null
+    });
+  }
+  
+  componentDidMount() {
+    this.getModuleById();
+    this.getProjectTypeById();       
+  }
 
-        return(
-        <div className={classes.root}>
-        <Paper style={{paddingTop: 15, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, marginTop: 15}} elevation={0}>
-        <TableRow>
-            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="center">{project.getName()} </TableCell>
-            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="center">{this.state.gradings}</TableCell>
-            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="center">Test</TableCell>
-            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Test</TableCell>
-            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Test</TableCell>
-            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Test</TableCell>
-            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align='center'>Test</TableCell>
-        </TableRow>)        
-        </Paper>
-        </div>
-        );
-    }
+  render() {
+    const { classes } = this.props;
+    const { module, project, projectType } = this.state;
+    return (
+      <TableRow>
+        <TableCell style={{width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">
+          {project.getName()} 
+        </TableCell>
+        <TableCell style={{width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">
+          { module ?
+            module.getName()
+            :
+            'k.A.'}
+        </TableCell>
+        <TableCell style={{width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">
+          { module ?
+            module.getEdvNumber()
+            :
+            'k.A.'}
+        </TableCell>
+        <TableCell style={{width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">
+          SemesterTest
+        </TableCell>
+        <TableCell style={{width: '14,3', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">
+          NoteTest
+        </TableCell>
+        <TableCell style={{width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">
+        { projectType ?
+            projectType.getNumberEcts()
+            :
+            'k.A.'}
+        </TableCell>
+        <TableCell style={{width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align='left'>
+        { projectType ?
+            projectType.getNumberSws()
+            :
+            'k.A.'}  
+        </TableCell>
+      </TableRow>
+    );
+  }
 }
 
 const styles = theme => ({
--- frontend/src/components/StudentGradingView.js
+++ frontend/src/components/StudentGradingView.js
@@ -1,101 +1,92 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import ProjectAdminAPI from '../api/ProjectAdminAPI';
-import { Button, Grid, Typography, withStyles } from '@material-ui/core';
+import { Button, Grid, withStyles } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { colors } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
+import TableContainer from '@material-ui/core/TableContainer';
 import StudentGradingEntry from './StudentGradingEntry';
 
 class StudentGradingView extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            projects: [],
-            error: null,
-            currentUserEmail: props.currentUserEmail
-        } 
-    }   
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+      error: null,
+      currentUserEmail: props.currentUserEmail
+    } 
+  }   
     
-    getRegisteredProjectsOfStudent = async() => {
-      let student = await ProjectAdminAPI.getAPI().getStudentByGoogleId(this.props.currentUserEmail)
-      //console.log(student)
-      console.log(ProjectAdminAPI.getAPI().getRegisteredProjectsOfStudent(student[0].getId()))
-      ProjectAdminAPI.getAPI().getRegisteredProjectsOfStudent(student[0].getId())
-      .then(projectNBOs => {
-        this.setState({
-          projects: projectNBOs,
-          filteredProjects: [...projectNBOs],
-          loadingInProgress: false,
-          error: null
-        });
-      }).catch(e => {
-        this.setState({
-          projects: [],
-          loadingInProgress: false,
-          error: e
-        })
-      });
+  getRegisteredProjectsOfStudent = async() => {
+    let student = await ProjectAdminAPI.getAPI().getStudentByGoogleId(this.props.currentUserEmail)
+    //console.log(student)
+    console.log(ProjectAdminAPI.getAPI().getRegisteredProjectsOfStudent(student[0].getId()))
+    ProjectAdminAPI.getAPI().getRegisteredProjectsOfStudent(student[0].getId())
+    .then(projectNBOs => {
       this.setState({
+        projects: projectNBOs,
+        filteredProjects: [...projectNBOs],
+        loadingInProgress: false,
+        error: null
+      });
+    }).catch(e => {
+      this.setState({
+        projects: [],
+        loadingInProgress: false,
+        error: e
+      })
+    });
+    this.setState({
       loadingInProgress: true,
       error: null
-      });
-    }   
-    componentDidMount() {
-      this.getRegisteredProjectsOfStudent();
-    }
+    });
+  }   
+  
+  componentDidMount() {
+    this.getRegisteredProjectsOfStudent();
+  }
 
-    render() {
-        const {classes} = this.props;
-        const {projects} = this.state;
-
-        return(
-        <div className={classes.root}>
-        <Paper style={{paddingTop: 15, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, marginTop: 15}} elevation={0}>
+  render() {
+    const {classes} = this.props;
+    const {projects} = this.state;
+    return(
+      <div>
         <Grid Container spacing={2}>
-            <Grid>
-                <Button style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} variant="contained">Leistungsübersicht</Button>
-            </Grid>
+          <Grid>
+              <Button style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} variant="contained">Leistungsübersicht</Button>
+          </Grid>
         </Grid>
-        <TableRow>
-                    <TableCell style={{width: '100%', paddingBottom: 0, paddingTop: 0, backgroundColor: colors.grey[100]}} colSpan={10}>
-                            <Box margin={1}>
-                                <Table size="small" aria-label="purchases">
-                                    <TableHead >
-                                        <TableRow>
-                                            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Projekt</TableCell>
-                                            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Modul</TableCell>
-                                            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">EDV-Nummer</TableCell>
-                                            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Semester</TableCell>
-                                            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="center">Note</TableCell>
-                                            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="center">ECTS</TableCell>
-                                            <TableCell style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="center">SWS</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}}>
-                                        {projects.length > 0 ? 
-                                                projects.map(project =>
-                                                  <StudentGradingEntry
-                                                  propproject={project}
-                                                  />                               
-                                                 ) :
-                                                    null
-                                        }             
-                                    </TableBody>
-                                </Table>
-                            </Box>
-                    </TableCell>    
-                </TableRow>
-        </Paper>
-        </div>
-        );
-    }
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="purchases">
+            <TableHead style={{backgroundColor: '#697bdb'}}>
+              <TableRow>
+                <TableCell style={{color: 'white', width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Projekt</TableCell>
+                <TableCell style={{color: 'white', width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Modul</TableCell>
+                <TableCell style={{color: 'white', width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">EDV</TableCell>
+                <TableCell style={{color: 'white', width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Semester</TableCell>
+                <TableCell style={{color: 'white', width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">Note</TableCell>
+                <TableCell style={{color: 'white', width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">ECTS</TableCell>
+                <TableCell style={{color: 'white', width: '14,3%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} colSpan={1} padding="none" align="left">SWS</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}}>
+              { projects.length > 0 ? 
+                projects.map(project =>
+                  <StudentGradingEntry key={project.getId()} project={project}/>)
+                :
+                null
+              }           
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  }
 }
 
 const styles = theme => ({
