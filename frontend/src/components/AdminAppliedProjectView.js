21bf00ba8025d394b1ca56d24ca7226686951a91
YusufGoegues <73108892+YusufGoegues@users.noreply.github.com>
2021-02-06 00:26:02 +0100

    Alle Projekte auf genehmigte Projekte umgeändert. Namen und Funktionen umgeschrieben.

--- frontend/src/components/AdminAppliedProjectView.js
+++ frontend/src/components/AdminAppliedProjectView.js
@@ -1,4 +1,4 @@
-import { Paper,} from '@material-ui/core';
+import { Paper, } from '@material-ui/core';
 import React from 'react';
 import ProjectAdminAPI from '../api/ProjectAdminAPI'
 import ProjectNBO from '../api/ProjectNBO';
@@ -11,17 +11,17 @@ import { withStyles, Button, List, ListItem, ListItemSecondaryAction, Typography
 
 class AdminAppliedProjectView extends React.Component {
 
-  constructor (props){
+  constructor(props) {
     super(props);
-    this.state={
-      projects:[],
+    this.state = {
+      projects: [],
       filteredProjects: [],
       projectFilter: '',
     }
   }
 
   getProjectsByStateNew = () => {
-    ProjectAdminAPI.getAPI().getProjectsByCurrentState("Neu").then(projectBOs => 
+    ProjectAdminAPI.getAPI().getProjectsByCurrentState("Neu").then(projectBOs =>
       this.setState({
         projects: projectBOs,
         filteredProjects: [...projectBOs],
@@ -42,65 +42,65 @@ class AdminAppliedProjectView extends React.Component {
       }),
       projectFilter: searchterm
     })
-}
+  }
 
-clearProjectFilter = () => {
+  clearProjectFilter = () => {
     this.setState({
       filteredProjects: [...this.state.projects],
       projectFilter: ''
     })
-}
-removeProject = (projectId) => {
-  console.log(this.state.filteredProjects[0])
-  console.log(projectId)
-  let newProjects= this.state.filteredProjects.filter(project => project.getId()!== projectId)
-  console.log(newProjects.length)
-  this.setState({
-      filteredProjects: this.state.filteredProjects.filter(project => project.getId()!== projectId)
-  })
-}
+  }
+  removeProject = (projectId) => {
+    console.log(this.state.filteredProjects[0])
+    console.log(projectId)
+    let newProjects = this.state.filteredProjects.filter(project => project.getId() !== projectId)
+    console.log(newProjects.length)
+    this.setState({
+      filteredProjects: this.state.filteredProjects.filter(project => project.getId() !== projectId)
+    })
+  }
 
-  render(){
-    const {projects, projectFilter, filteredProjects}=this.state;
-    return(
-      <div> 
-        <Paper style={{paddingTop: 15, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, marginTop: 15}} elevation={0}>
+  render() {
+    const { projects, projectFilter, filteredProjects } = this.state;
+    return (
+      <div>
+        <Paper style={{ paddingTop: 15, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, marginTop: 15 }} elevation={0}>
           <Grid container spacing={1} justify='flex-start' alignItems='center'>
-            <Button style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} variant="contained">
-              Eingereichte Projekte
+            <Button style={{ width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10 }} variant="contained">
+              Alle eingereichten Projekte
             </Button>
           </Grid>
-          <Grid style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}}>
-                    <TextField 
-                        autoFocus type='text' 
-                        value={projectFilter} 
-                        onChange={this.filterProjects}
-                        InputProps={{
-                            endAdornment: <InputAdornment position='end'>
-                                <IconButton onClick={this.clearProjectFilter}>
-                                    <HighlightOffIcon/>
-                                </IconButton>
-                            </InputAdornment>
-                        }}
-                    />
-                </Grid>
-          <Grid style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}}>
+          <Grid style={{ width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10 }}>
+            <TextField
+              autoFocus type='text'
+              value={projectFilter}
+              onChange={this.filterProjects}
+              InputProps={{
+                endAdornment: <InputAdornment position='end'>
+                  <IconButton onClick={this.clearProjectFilter}>
+                    <HighlightOffIcon />
+                  </IconButton>
+                </InputAdornment>
+              }}
+            />
+          </Grid>
+          <Grid style={{ width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10 }}>
             <Typography>
               Hier können Sie eingereichte Projekte genehmigen oder ablehnen:
             </Typography>
             {
-            projects.length > 0 ?
-              filteredProjects.map (project => 
-                <AdminAppliedProjectEntry currentUserEmail={this.props.currentUserEmail} key={project.getId()} project={project} onDelete={() => this.removeProject(project.getId())}/>)
-                
-              :
-              "Test"
+              projects.length > 0 ?
+                filteredProjects.map(project =>
+                  <AdminAppliedProjectEntry currentUserEmail={this.props.currentUserEmail} key={project.getId()} project={project} onDelete={() => this.removeProject(project.getId())} />)
+
+                :
+                "Test"
             }
           </Grid>
         </Paper>
       </div>
     )
-        
+
 
 
   }
--- frontend/src/components/AdminView.js
+++ frontend/src/components/AdminView.js
@@ -1,4 +1,4 @@
-import {Paper}  from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import React from 'react';
 import ProjectAdminAPI from '../api/ProjectAdminAPI'
 import ProjectNBO from '../api/ProjectNBO';
@@ -11,17 +11,17 @@ import { withStyles, Button, List, ListItem, ListItemSecondaryAction, Typography
 
 class AdminView extends React.Component {
 
-  constructor (props){
+  constructor(props) {
     super(props);
-    this.state={
-      projects:[],
+    this.state = {
+      projects: [],
       filteredProjects: [],
       projectFilter: '',
     }
   }
 
-  getAllProjects = () => {
-    ProjectAdminAPI.getAPI().getAllProjects().then(projectBOs => 
+  getProjectsByStateAccepted = () => {
+    ProjectAdminAPI.getAPI().getProjectsByCurrentState("Genehmigt").then(projectBOs =>
       this.setState({
         projects: projectBOs,
         filteredProjects: [...projectBOs],
@@ -30,7 +30,7 @@ class AdminView extends React.Component {
   }
 
   componentDidMount() {
-    this.getAllProjects();
+    this.getProjectsByStateAccepted();
   }
 
   filterProjects = event => {
@@ -42,66 +42,66 @@ class AdminView extends React.Component {
       }),
       projectFilter: searchterm
     })
-}
+  }
 
-clearProjectFilter = () => {
+  clearProjectFilter = () => {
     this.setState({
       filteredProjects: [...this.state.projects],
       projectFilter: ''
     })
-}
+  }
 
-removeProject = (projectId) => {
-  console.log(this.state.filteredProjects[0])
-  console.log(projectId)
-  let newProjects= this.state.filteredProjects.filter(project => project.getId()!== projectId)
-  console.log(newProjects.length)
-  this.setState({
-      filteredProjects: this.state.filteredProjects.filter(project => project.getId()!== projectId)
-  })
-}
+  removeProject = (projectId) => {
+    console.log(this.state.filteredProjects[0])
+    console.log(projectId)
+    let newProjects = this.state.filteredProjects.filter(project => project.getId() !== projectId)
+    console.log(newProjects.length)
+    this.setState({
+      filteredProjects: this.state.filteredProjects.filter(project => project.getId() !== projectId)
+    })
+  }
 
-  render(){
-    const {projects, projectFilter, filteredProjects}=this.state;
-    return(
-      <div> 
-        <Paper style={{paddingTop: 15, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, marginTop: 15}} elevation={0}>
+  render() {
+    const { projects, projectFilter, filteredProjects } = this.state;
+    return (
+      <div>
+        <Paper style={{ paddingTop: 15, paddingLeft: 15, paddingRight: 15, paddingBottom: 15, marginTop: 15 }} elevation={0}>
           <Grid container spacing={1} justify='flex-start' alignItems='center'>
-            <Button style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}} variant="contained">
-              Alle Projekte
+            <Button style={{ width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10 }} variant="contained">
+              Alle genehmigten Projekte
             </Button>
           </Grid>
-          <Grid style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}}>
-                    <TextField 
-                        autoFocus type='text' 
-                        value={projectFilter} 
-                        onChange={this.filterProjects}
-                        InputProps={{
-                            endAdornment: <InputAdornment position='end'>
-                                <IconButton onClick={this.clearProjectFilter}>
-                                    <HighlightOffIcon/>
-                                </IconButton>
-                            </InputAdornment>
-                        }}
-                    />
-                </Grid>
-          <Grid style={{width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10}}>
+          <Grid style={{ width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10 }}>
+            <TextField
+              autoFocus type='text'
+              value={projectFilter}
+              onChange={this.filterProjects}
+              InputProps={{
+                endAdornment: <InputAdornment position='end'>
+                  <IconButton onClick={this.clearProjectFilter}>
+                    <HighlightOffIcon />
+                  </IconButton>
+                </InputAdornment>
+              }}
+            />
+          </Grid>
+          <Grid style={{ width: '100%', paddingBottom: 10, paddingLeft: 10, marginTop: 10 }}>
             <Typography>
               Hier können Sie alle Projekte einsehen oder bearbeiten:
             </Typography>
             {
-            projects.length > 0 ?
-              filteredProjects.map (project => 
-                <AdminViewEntry currentUserEmail={this.props.currentUserEmail} key={project.getId()} project={project} onDelete={() => this.removeProject(project.getId())}/>)
+              projects.length > 0 ?
+                filteredProjects.map(project =>
+                  <AdminViewEntry currentUserEmail={this.props.currentUserEmail} key={project.getId()} project={project} onDelete={() => this.removeProject(project.getId())} />)
 
-              :
-              "Test"
+                :
+                "Test"
             }
           </Grid>
         </Paper>
       </div>
     )
-        
+
 
 
   }
