66e6fd42b82ce8a39ac1b20b26a05e3eec27e7b8
Christina Kaden <ck171@hdm-stuttgart.de>
2021-02-07 22:49:44 +0100

    Methode getGradingByParticipationOfStudent in DocentParticipantEntry.js hinzugefügt für das Anzeigen der Note, falls Bewertung für eine Teilnahme bereits vorliegt

--- frontend/src/components/dialogs/DocentParticipantEntry.js
+++ frontend/src/components/dialogs/DocentParticipantEntry.js
@@ -11,7 +11,8 @@ class DocentParticipantEntry extends React.Component {
 
         this.state = {
             student: props.student,
-            grade: null
+            grade: null,
+            grading: null,
         }
         this.baseState = this.state;
         console.log(this.state.student.getId())
@@ -27,7 +28,17 @@ class DocentParticipantEntry extends React.Component {
         .then(grading => {
             this.setState(this.baseState)
         })
+    }
 
+    getGradingByParticipationOfStudent = async() => {
+        let participation = await ProjectAdminAPI.getAPI().getParticipationByStudent(this.state.student.getId())
+        console.log(participation.getId())
+        ProjectAdminAPI.getAPI().getGradingByParticipation(participation.getId())
+        .then(gradingBO => {
+            this.setState({
+                grading: gradingBO
+            })
+        })
     }
 
     handleGradingButton = () => {
@@ -40,15 +51,29 @@ class DocentParticipantEntry extends React.Component {
         });
     }
 
+    componentDidMount() {
+        this.getGradingByParticipationOfStudent();
+    }
+
     render() {
         const { classes } = this.props;
-        const { student, grade } = this.state;
+        const { student, grade, grading } = this.state;
+        console.log(grading)
         return (
             <div className={classes.root}>
                 <Grid container spacing={1} xs={12}>
                     <Grid item style={{marginBottom: 10, marginTop: 10}} xs={6}>
-                        <Typography className={classes.heading} >
+                        <Typography className={classes.heading}>
                             <b>{student.getName()}</b>
+                        </Typography>
+                    </Grid>
+                    <Grid>
+                        <Typography className={classes.heading}>
+                            { grading ?
+                            <b>Note: {grading[0].getGrade()}</b>
+                            :
+                            'noch keine Bewertung'
+                            }
                         </Typography>
                     </Grid>
                     <Grid>
